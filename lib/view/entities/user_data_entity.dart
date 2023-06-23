c09ef7c6952028e6298fc57d822641fe652f4901
qolbudr <qolbu.dzikru30@gmail.com>
2023-06-23 11:35:44 +0700

    create userdata entity

---
+++ lib/view/entities/user_data_entity.dart
@@ -0,0 +1,12 @@
+class UserData {
+  int? id;
+  String? name;
+  String? email;
+  String? role;
+  String? activeSubscription;
+  String? subscriptionType;
+  DateTime? subscriptionExp;
+  int? subscriptionRemaining;
+
+  UserData({this.id, this.name, this.email, this.role, this.activeSubscription, this.subscriptionType, this.subscriptionExp, this.subscriptionRemaining});
+}
