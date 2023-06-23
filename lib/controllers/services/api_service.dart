504feac00da99e3ffd15a08b1ceb487c86d55734
qolbudr <qolbu.dzikru30@gmail.com>
2023-06-23 11:35:25 +0700

    create api service

---
+++ lib/controllers/services/api_service.dart
@@ -0,0 +1,29 @@
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+import 'package:snplay/constant.dart';
+
+class ApiService {
+  Future<Map<String, dynamic>> get(String url) async {
+    try {
+      final response = await http.get(Uri.parse(url), headers: {'X-API-KEY': apiKey});
+      String body = response.body;
+      return jsonDecode(body);
+    } catch (e) {
+      rethrow;
+    }
+  }
+
+  Future<Map<String, dynamic>> post(String url, Map<String, dynamic> data) async {
+    try {
+      final response = await http.post(
+        Uri.parse(url),
+        headers: {'X-API-KEY': apiKey, 'Content-Type': 'application/json'},
+        body: jsonEncode(data),
+      );
+      String body = response.body;
+      return jsonDecode(body);
+    } catch (e) {
+      rethrow;
+    }
+  }
+}
